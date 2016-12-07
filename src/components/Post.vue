8057cdd141b241055102afa430872081ffb4cd67
Rizky <rizkyshofyan@gmail.com>
2016-12-07 23:20:33 +0700

    fixing content

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -4,9 +4,12 @@
       <div class="columns">
         <div class="column is-two-third content">
           <div class="box">
+            <h1 class="title is-2">{{ mainContent.title }}</h1>
+            <div class="sub-title meta">Posted by {{ mainContent.writer.full_name}} at {{ moment(mainContent.created_at).fromNow() }}</div>
             <div class="content">
-              <h1 class="title is-1">{{ mainContent.title }}</h1>
-              
+              <div v-if="mainContent.type === 'video'" class="video-container">
+                <iframe width="560" height="315" :src="generateVideoUrl(mainContent.content.key)" frameborder="0" allowfullscreen></iframe>
+              </div>
             </div>
           </div>
         </div>
@@ -112,6 +115,16 @@ export default {
       }, (response) => {
       })
     },
+    reloadStories: function () {
+      this.$http.get('http://api.news.nixia.tech/post/show?id=' + this.$route.params.slug)
+      .then((response) => {
+        if (response.body.meta.status === true) {
+          this.mainContent = response.body.data
+          console.log(response.body)
+        }
+      }, (response) => {
+      })
+    },
     moment: function (args) {
       return Moment(args)
     },
@@ -122,12 +135,31 @@ export default {
         }
       }
       return
+    },
+    generateVideoUrl: function (args) {
+      return 'https://www.youtube.com/embed/' + args
     }
   },
   watch: {
     '$route' (to, from) {
-      this.fetchStories()
+      this.reloadStories()
     }
   }
 }
 </script>
+<style lang="scss">
+  .video-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 35px;
+    height: 0;
+    overflow: hidden;
+    iframe {
+      position: absolute;
+      top:0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+</style>
